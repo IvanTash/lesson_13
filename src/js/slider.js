9f3f2cb610c4402c66ed685e2fe9fe2ee70a0544
IvanTash <Stinky88@mail.ru>
2019-03-07 23:44:57 +0500

    Добавлена валидация форм

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,5 +1,51 @@
 new WOW().init(); //Инициализация wow.js
-$(document).ready(function() { // инициализация слайдера
+$(document).ready(function() { 
+  /* Валидиация формы brif*/
+  $('#brif-form').validate({
+    rules: {
+      username: {
+        required: true,
+        minlength: 3,
+        maxlength: 15},
+      email: {
+        required: true,
+        email: true,
+      },
+      phone: {
+        required: true,
+      }
+    },
+    messages: {
+      username: "Заполните поле",
+      email: {
+        required: "Заполните поле",
+        email: "Введите корректный email",
+      },
+      phone: "Укажите телефон",
+    }
+  })
+  /* Валидация формы offer */
+  $('#offer-form').validate({
+    rules: {
+      username: {
+        required: true,
+        minlength: 3,
+        maxlength: 15,
+      },
+      phone: {
+        required: true
+      },
+    },
+    messages: {
+      username: "Заполните поле",
+      phone: "Укажите телефон",
+    }
+  });
+
+  /* Маска телефона */
+  $('.phone').mask('8 (999) 999-99-99');
+
+ /*  инициализация слайдера */
   $('.slider').slick( {
     infinite: true,
   slidesToShow: 3,
